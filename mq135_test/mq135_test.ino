33e653598a60281eb70b4f4f6136a7827a3506bc
juanmarinmo <jummarinmo@unal.edu.co>
2019-10-24 17:51:13 -0500

    De ahora en adelante se trabajará con el framework de espressif

--- mq135_test/mq135_test.ino
+++ mq135_test/mq135_test.ino
@@ -1,31 +1,31 @@
-#define sensor 4
+int sensor = 36;
+
+
+
+
+
 
 
 float RL = 20000;
 float Ro = 776500; //valor obtenido en caracterización
 
 
-//long RL = 10;
-//long Ro = 76.63;
-
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+//  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_0db);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-//float v = analogRead(sensor);
+//float v = 5.0 * analogRead(sensor) / 1024;
 
-float v = 5.0 * analogRead(sensor) / 1024;
 
-float Rs = RL * (5 - v) / v;
+float v = 3.3 * analogRead(sensor) / 4095;
+
+float Rs = RL * (3.3 - v) / v;
 
 float ratio = Rs/Ro;
 
@@ -34,9 +34,9 @@ float ppm = 115.1 * pow(ratio,-2.924);
 
 
 
-Serial.println(ppm);
+Serial.println(analogRead(sensor));
 
-delay(500);
+delay(1000);
 
 
 }
